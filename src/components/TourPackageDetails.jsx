1e7bc128749a4b6bd25fb64cf86eb5fdf2d44998
Abdulmujib <asiyanbiakorede@gmail.com>
2025-04-21 08:54:56 +0100

    Tour package details page

--- src/components/TourPackageDetails.jsx
+++ src/components/TourPackageDetails.jsx
@@ -1,20 +1,188 @@
 import { useParams } from "react-router-dom";
-import { tourPackages } from "../../utils/constants";
 import Layout from "./Layout";
+import { packageDetails } from "../../utils/constants";
 import Testimonial from "./Testimonial";
+import { useNavigate } from "react-router-dom";
+import Calendar from "react-calendar";
+import { useState } from "react";
+// import "react-calendar/dist/Calendar.css";
+
 function TourPackageDetails() {
+  const [date,setDate] = useState(new Date())
+  const navigate = useNavigate()
   const { id } = useParams();
-  const packageDetail = tourPackages.find((item) => item.id === Number(id));
-  console.log({ packageDetail }, { id });
+  const packageDetail = packageDetails.find((item) => item.id === Number(id));
+  console.log({date});
   return (
     <Layout>
-      <div>
-        TourPackageDetails for package {id}
-        <p>{packageDetail.date}</p>
-        <p>{packageDetail.about}</p>
-        <img src={packageDetail.img} alt="" />
+      <div
+        onClick={() => navigate(-1)}
+        className="my-10 miniLaptop:mx-40 aboutResponsive:mx-20 mx-[1rem] flex gap-2 font-semibold cursor-pointer w-fit"
+      >
+        <img src="/img/Vector (1).svg" alt="" />
+        <span>back</span>
       </div>
-      <Testimonial/>
+      <div className=" aboutResponsive:mx-20 miniLaptop:mx-40 mx-[1rem] ">
+        <section className="flex flex-col largerScreen:flex-row gap-10 w-full">
+          <div className="miniLaptop:w-[50%]  w-[100%] largerScreen:w-[40%] flex flex-col justify-center gap-5">
+            <img
+              className=""
+              src={packageDetail.tourImage.img1}
+              alt=""
+            />
+            <div className="flex justify-between">
+              <img
+                className=" w-[32%] "
+                src={packageDetail.tourImage.img2}
+                alt=""
+              />
+              <img
+                className=" w-[32%] "
+                src={packageDetail.tourImage.img3}
+                alt=""
+              />
+              <img
+                className=" w-[32%] "
+                src={packageDetail.tourImage.img4}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className=" largerScreen:w-[50%] w-[100%] flex flex-col gap-2">
+            <p className="font-extrabold mobileMax:text-5xl text-3xl ">{packageDetail.title}</p>
+            <p className="font-semibold text-2xl">
+              from{" "}
+              <span className="font-extrabold text-3xl text-tr-orange">
+                34 $
+              </span>{" "}
+            </p>
+            <p className="text-[#333333] text-lg">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book
+            </p>
+            <div className="flex flex-col">
+              <span className="font-semibold text-lg  text-[#333333]">
+                Select a date
+              </span>
+              <Calendar
+                value={date}
+                onChange={setDate}
+                className="flex flex-col  border-[1px] border-[#33333333] p-3 gap-3 text-sm w-fit"
+                calendarType="gregory"
+                tileClassName="flex w-fit  mx-0 px-0"
+              />
+            </div>
+            <div>
+              <p className="font-semibold text-lg text-[#333333] ">Time</p>
+              <main className="flex pl-6 pr-5 py-3 border-[1px] border-[#33333333] rounded-lg w-fit gap-3">
+                <input
+                  className="outline-none"
+                  placeholder="Select the time"
+                  type="text"
+                />
+                <img className="w-4" src="/img/Group.svg" alt="" />
+              </main>
+            </div>
+            <button className="bg-tr-orange rounded-full w-fit text-white py-[10px] px-[92px]">
+              Buy Now
+            </button>
+          </div>
+        </section>
+        <section className="largerScreen:w-[50%] laptop:w-[90%] w-[100%] my-20 flex flex-col gap-6">
+          <header className="font-extrabold text-3xl">Details</header>
+          <p className="text-lg">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <main className="flex flex-col gap-5">
+            <div className="w-fit flex gap-3 items-center">
+              <img src="/img/Layer 1 (3).svg" alt="" />
+              <p className="font-bold text-lg">
+                Number of group:{" "}
+                <span className="font-medium text-lg">15-30</span>
+              </p>
+            </div>
+            <div className="w-fit flex gap-3 items-center">
+              <img src="/img/Layer 2 (3).svg" alt="" />
+              <p className="font-bold text-lg">
+                Duration:{" "}
+                <span className="font-medium text-lg">
+                  15 hours and 45 minutes
+                </span>
+              </p>
+            </div>
+            <div className="w-fit flex gap-3 items-center">
+              <img src="/img/ci_location.svg" alt="" />
+              <p className="font-bold text-lg">
+                Departuring and arriving areas:{" "}
+                <span className="font-medium text-lg">Lucca</span>
+              </p>
+            </div>
+            <div className="w-fit flex gap-3 items-center">
+              <img src="/img/Layer 1 (4).svg" alt="" />
+              <p className="font-bold text-lg">
+                Guide Service:{" "}
+                <span className="font-medium text-lg">Included</span>
+              </p>
+            </div>
+            <div className="w-fit flex gap-3 items-center">
+              <img src="/img/Layer 2 (4).svg" alt="" />
+              <p className="font-bold text-lg">
+                Language:{" "}
+                <span className="font-medium text-lg">English,Italian</span>
+              </p>
+            </div>
+            <div className="w-fit flex gap-3 items-center">
+              <img src="/img/Layer 1 (5).svg" alt="" />
+              <p className="font-bold text-lg">
+                Entry fees:{" "}
+                <span className="font-medium text-lg">Lorem ipsum</span>
+              </p>
+            </div>
+            <div className="w-fit flex gap-3 items-center">
+              <img src="/img/Layer 1 (2).svg" alt="" />
+              <p className="font-bold text-lg">
+                Transportation: <span className="font-medium text-lg">Bus</span>
+              </p>
+            </div>
+          </main>
+        </section>
+        <section>
+          <div className="  flex justify-between items-center mb-10 laptop:mb-14">
+            <p className=" font-extrabold text-xl laptop:text-3xl ">Gallery</p>
+            <div className="flex gap-3 ">
+              <img
+                className="py-4 px-5  bg-[#EFEFEF] rounded-full"
+                src="/img/Vector (3).png"
+                alt=""
+              />
+              <img
+                className="py-4 px-5 bg-tr-orange rounded-full"
+                src="/img/Vector (4).png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="flex flex-col laptop:flex-row w-full gap-5 ">
+            <img className=" w-[100%] laptop:w-[45%]" src={packageDetail.gallery.img1} alt="" />
+            <div className="w-[100%] laptop:w-[25%] flex justify-between flex-row laptop:flex-col laptop:gap-0 gap-3  ">
+              <img className="laptop:w-full w-[49%]" src={packageDetail.gallery.img2} alt="" />
+              <img className="laptop:w-full w-[49%]" src={packageDetail.gallery.img3} alt="" />
+            </div>
+            <img className="laptop:w-[25%] w-[100%] " src={packageDetail.gallery.img4} alt="" />
+          </div>
+        </section>
+      </div>
+      <Testimonial />
     </Layout>
   );
 }
--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 export const tourPackages = [
   {
     id: 1,
-    img: "/img/Rectangle 16 (5).png",
+    img: "/img/Rectangle 16 (6).png",
     date: "EVERY DAY",
     people: "3-10 PP",
     about:
@@ -10,7 +10,7 @@ export const tourPackages = [
   },
   {
     id: 2,
-    img: "/img/Rectangle 16 (6).png",
+    img: "/img/Rectangle 16 (5).png",
     date: "MONDAY",
     people: "10-30 PP",
     about:
@@ -19,7 +19,7 @@ export const tourPackages = [
   },
   {
     id: 3,
-    img: "/img/Rectangle 16 (7).png",
+    img: "/img/Rectangle 16 (8).png",
     date: "TO BE DECIDED",
     people: "10-50 PP",
     about:
@@ -28,11 +28,77 @@ export const tourPackages = [
   },
   {
     id: 4,
-    img: "/img/Rectangle 16 (4).png",
+    img: "/img/Rectangle 16 (7).png",
     date: "TO BE DECIDED",
     people: "5-10 PP",
     about:
       "Visit the beautiful Siena and the cities that surround it to experience ...",
     title: "Siena and Surroundings",
+  },
+];
+export const packageDetails = [
+  {
+    id:1,
+    tourImage: {
+      img1: "/img/Rectangle 39 (1).png",
+      img2: "/img/Rectangle 40.png",
+      img3: "/img/Rectangle 41.png",
+      img4: "/img/Rectangle 42.png",
+    },
+    title: "Lucca Bike Tour",
+    gallery: {
+      img1: "/img/Rectangle 43.png",
+      img2: "/img/Rectangle 44.png",
+      img3: "/img/Rectangle 45.png",
+      img4: "/img/Rectangle 46.png",
+    },
+  },
+  {
+    id:2,
+    tourImage: {
+      img1: "/img/Rectangle 39 (2).png",
+      img2: "/img/Rectangle 40 (1).png",
+      img3: "/img/Rectangle 41 (1).png",
+      img4: "/img/Rectangle 42 (1).png",
+    },
+    title: "Wine tasting In Tuscany",
+    gallery: {
+      img1: "/img/Rectangle 43 (1).png",
+      img2: "/img/Rectangle 44 (1).png",
+      img3: "/img/Rectangle 45 (1).png",
+      img4: "/img/Rectangle 46 (1).png",
+    },
+  },
+  {
+    id:3,
+    tourImage: {
+      img1: "/img/Rectangle 39 (3).png",
+      img2: "/img/Rectangle 40 (2).png",
+      img3: "/img/Rectangle 41 (2).png",
+      img4: "/img/Rectangle 42 (2).png",
+    },
+    title: "Cinque Terre Tour",
+    gallery: {
+      img1: "/img/Rectangle 43 (2).png",
+      img2: "/img/Rectangle 44 (2).png",
+      img3: "/img/Rectangle 45 (2).png",
+      img4: "/img/Rectangle 46 (2).png",
+    },
+  },
+  {
+    id:4,
+    tourImage: {
+      img1: "/img/Rectangle 39 (4).png",
+      img2: "/img/Rectangle 40 (3).png",
+      img3: "/img/Rectangle 41 (3).png",
+      img4: "/img/Rectangle 42 (3).png",
+    },
+    title: "Siena and Surroundings",
+    gallery: {
+      img1: "/img/Rectangle 43 (3).png",
+      img2: "/img/Rectangle 44 (3).png",
+      img3: "/img/Rectangle 45 (3).png",
+      img4: "/img/Rectangle 46 (3).png",
+    },
   },
 ];
